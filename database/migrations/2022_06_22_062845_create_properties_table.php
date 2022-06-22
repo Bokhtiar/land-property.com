ef5309a0ee6cdc0a84073c9608512a909de3bd63
Bokhtiar Tushar <bokhtiartoshar1@gmail.com>
2022-06-22 12:39:54 +0600

    property create database schema

---
+++ database/migrations/2022_06_22_062845_create_properties_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePropertiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id('property_id');
+            $table->string('title')->require();
+            $table->longText('body')->require();
+            $table->string('image');
+            $table->integer('category_id')->require();
+            $table->string('location')->require();
+            $table->string('phone')->require();
+            $table->integer('price');
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('properties');
+    }
+}
